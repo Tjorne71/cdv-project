f9ec375d2c56220c0c5de074808867ff7522da35
Laurits Brok <lauritsbrok@gmail.com>
2023-07-25 18:42:19 +0200

    Added animation

--- src/components/county.jsx
+++ src/components/county.jsx
@@ -1,15 +1,22 @@
 "use client";
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { motion } from "framer-motion"
 
 export default function County({ d, county, color, countyClicked}) {
   const [hover, setHover] = useState(false);
+  const [currentColor, setCurrentColor] = useState(color);
+
+
+  useEffect(() => {
+    setCurrentColor(color);
+  }, [color]);
+
   return (
     <>
       <motion.path
         stroke="black"
         strokeWidth={`${hover ? "1" : "0.1"}`}
-        fill={color}
+        fill={currentColor}
         d={d}
         onMouseLeave={() => {
           setHover(false);
@@ -19,6 +26,12 @@ export default function County({ d, county, color, countyClicked}) {
         }}
         onClick={() => {
           countyClicked(county);
+        }}
+        animate={{ 
+          fill: currentColor,
+        }}
+        transition={{
+          duration: 0.4, // Total duration of the transition // Duration of scale animation
         }}
       />
     </>
