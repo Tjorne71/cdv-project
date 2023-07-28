cbdeabe074e24b9c4636fa0e74b9e5b00d5374f2
Tjorne71 <emtj@itu.dk>
2023-07-28 19:12:50 +0200

    fixed slider bug

--- src/components/customSlider.jsx
+++ src/components/customSlider.jsx
@@ -1,15 +1,20 @@
 "use client";
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Slider from "@mui/material/Slider";
 
 export default function CustomSlider({ label, onChangeCommitted, min, max, width, setIsSentenceVisible, value, showLabel}) {
   const [sliderValue, setSliderValue] = useState(value)
+
+  useEffect(() => {
+    setSliderValue(value);
+  }, [value]);
 
   function onSliderChange(event) {
     const value = event.target.value;
     setSliderValue(value)
     setIsSentenceVisible(false)
   }
+
   return (
     <div style={{width:width+'px'}} className={`flex flex-col`}>
       <h1 className="font-Montserrat text-base">{label}</h1>
@@ -17,9 +22,9 @@ export default function CustomSlider({ label, onChangeCommitted, min, max, width
         size="small" 
         getAriaLabel={() => "Slider"} 
         onChange={onSliderChange} 
-        onChangeCommitted={() => {onChangeCommitted(sliderValue)}} 
-        defaultValue={value} 
-        value={value}
+        onChangeCommitted={() => {onChangeCommitted(sliderValue)}}
+        defaultValue={sliderValue}
+        value={sliderValue}
         min={min} 
         max={max}
         valueLabelDisplay={showLabel ? "auto" : "off"}
