2dc17535967c21f81f096d4c8fa66c4b90ba44e4
Tjorne71 <emtj@itu.dk>
2023-07-29 19:54:04 +0200

    added explain to colorLegend

--- src/components/colorLegend.jsx
+++ src/components/colorLegend.jsx
@@ -17,7 +17,19 @@ export default function ColorLegend({ colorFunction }) {
 
         return <div key={level} style={style} className={`h-2 flex items-center justify-center w-6 text-base font-extrabold font-Montserrat text-center text-primary ${index == 0 ? "rounded-l-full" : ""} ${index == levels.length - 1 ? "rounded-r-full" : ""}`}></div>;
       })}
-      <Tooltip title={<span className="text-base">explain</span>}>
+      <Tooltip title={
+                <div className="flex flex-col items-center">
+                  <p>Fires a categories based on there size from A - G</p>
+                  <div className="flex flex-row m-auto items-center mx-auto">
+                    {levels.map((level, index) => {
+                      return (
+                      <div key={level} style={{backgroundColor : colorFunction(index + 2)}} className={`h-6 flex items-center justify-center w-6 mr-1 text-base font-extrabold font-Montserrat text-center text-primary`}>
+                        {level}
+                      </div>
+                    )})}
+                  </div>
+                </div>
+      }>
         <IconButton>
           <InfoIcon sx={{ color: "white" }} />
         </IconButton>
