e694113cc79830f663c92f02dabafee0b57a021f
Tjorne71 <emtj@itu.dk>
2023-07-29 18:09:45 +0200

    added state text on map

--- src/components/customMap.jsx
+++ src/components/customMap.jsx
@@ -8,45 +8,21 @@ import County from "@/components/county";
 import CircularProgress from "@mui/material/CircularProgress";
 import { dataMapper } from "@/util/dataMapper";
 import ColorLegend from "@/components/colorLegend";
-import Modal from '@mui/material/Modal';
-import Box from '@mui/material/Box';
-import Typography from '@mui/material/Typography';
 
-export default function CustomMap({ focusYear, focusMonth, height, width, setFocusCounty, setFireSentence, setFireTotal, setIsSentenceVisible}) {
+export default function CustomMap({ focusYear, focusMonth, height, width, setFocusCounty, setFireSentence, setFireTotal, setIsSentenceVisible }) {
   const usData = countiesUs;
   const counties = feature(usData, usData.objects.counties);
   const wildfireData = dataMapper(wildfireJson, counties);
-  const [open, setOpen] = React.useState(false);
-  const handleOpen = () => setOpen(true);
-  const handleClose = () => setOpen(false);
-
-  const style = {
-    position: 'absolute',
-    top: '50%',
-    left: '50%',
-    transform: 'translate(-50%, -50%)',
-    width: 800,
-    bgcolor: 'background.paper',
-    border: '2px solid #000',
-    boxShadow: 24,
-    p: 4,
-  };
-
   const projection = d3
     .geoAlbersUsa()
     .scale(900)
     .translate([width / 2, height / 2]);
-  const geoPath = d3
-    .geoPath()
-    .projection(projection);
+  const geoPath = d3.geoPath().projection(projection);
   const focusWildFireData = wildfireData.filter((fire) => fire.month == focusMonth && fire.year == focusYear);
   const usStatesPath = geoPath(mesh(usData, usData.objects.states, (a, b) => a !== b));
 
-  const reds = d3
-  .scaleSequential()
-  .domain([1, 8])
-  .interpolator(d3.interpolateReds);
-  
+  const reds = d3.scaleSequential().domain([1, 8]).interpolator(d3.interpolateReds);
+
   function countyClicked(county, fireSize) {
     setFocusCounty(county.properties.name);
     setFireSentence(getFireSentence(fireSize));
@@ -58,53 +34,29 @@ export default function CustomMap({ focusYear, focusMonth, height, width, setFoc
       <svg height={height} width={width}>
         <g fill="none" stroke="none" strokeLinejoin="round" strokeLinecap="round">
           {counties.features.map((county) => {
-            return <County key={county.id} color={"#fff5f0"} d={geoPath(county)} county={county} countyClicked={countyClicked} fireSize={0}/>;
+            return <County key={county.id} color={"#fff5f0"} d={geoPath(county)} county={county} countyClicked={countyClicked} fireSize={0} />;
           })}
           <path stroke="black" strokeWidth="0.6" d={usStatesPath}></path>
         </g>
         <g fill="none" stroke="none" strokeLinejoin="round" strokeLinecap="round">
           {focusWildFireData.map((fire) => {
             const color = fire.fireSize == 0 ? "#fff5f0" : reds(getFireValue(fire.fireSize));
-            return <County key={"f"+fire.id} color={color} d={geoPath(fire.county)} county={fire.county} countyClicked={countyClicked} fireSize={fire.fireSize}/>;
+            return <County key={"f" + fire.id} color={color} d={geoPath(fire.county)} county={fire.county} countyClicked={countyClicked} fireSize={fire.fireSize} />;
           })}
           <path stroke="black" strokeWidth="1" d={usStatesPath}></path>
+          {/* Add text labels for each state */}
+          {usData.objects.states.geometries.map((state) => {
+            const stateFeature = feature(usData, state);
+            const centroid = geoPath.centroid(stateFeature);
+            return (
+              <text key={state.id} x={centroid[0]} y={centroid[1]} textAnchor="middle" dominantBaseline="central" fill="black" fontSize="6px" stroke="white" strokeWidth={1} strokeOpacity={0.8} className="font-Montserrat" paintOrder="stroke">
+                {state.properties.name}
+              </text>
+            );
+          })}
         </g>
       </svg>
-      <div className="flex">
-        <ColorLegend colorFunction={reds}/>
-        <a href="#" onClick={handleOpen} className="ml-auto mr-2 text-sm p-2 underline hover:text-gray-300 transition-all duration-500 ease-in-out">Data Source</a>
-        <Modal
-          open={open}
-          onClose={handleClose}
-          aria-labelledby="modal-modal-title"
-          aria-describedby="modal-modal-description"
-        >
-          <Box sx={style}>
-            <Typography id="modal-modal-title" variant="h6" component="h2">
-              Data source
-            </Typography>
-            <Typography id="modal-modal-description" sx={{ mt: 2 }}>
-              
-              <p>All data in this project was sourced from the dataset "1.88 Million US Wildfires".</p>
-              <br/>
-              <p>We have extracted our own dataset using using SQL which is used to generate the visualizations.</p>
-              <br/>
-              <p>The datasets can be downloaded here in JSON:</p>
-              <a className="text-blue-500" target="_blank" href="https://drive.google.com/file/d/1A81bOo2kZDV1A9HY4_Xo9opsrM6gnGju/view?usp=sharing">1. Monthly Fires Per County </a>
-              - The sum of all fires in every county every single month from 1992-2015.
-              <br/>
-              <a className="text-blue-500" target="_blank" href="https://drive.google.com/file/d/1nG-w1_LU-WmriZ84J-3xZT0womRlOALH/view?usp=sharing">2. Monthly Fires in the entire US </a>
-              - The sum of all fires in the United Stats every single month from 1992-2015.
-              <br/><br/>
-              <p>The entire dataset can be downloaded from <a className="text-blue-500" target="_blank" href="https://www.kaggle.com/datasets/rtatman/188-million-us-wildfires">Kaggle.</a></p>
-              <br/>
-              <h6 className="font-bold">Citation:</h6>
-              <p>Short, Karen C. 2017. Spatial wildfire occurrence data for the United States, 1992-2015 [FPA_FOD_20170508]. 
-              4th Edition. Fort Collins, CO: Forest Service Research Data Archive. https://doi.org/10.2737/RDS-2013-0009.4</p>
-            </Typography>
-          </Box>
-        </Modal>
-      </div>
+      <ColorLegend colorFunction={reds} />
     </main>
   );
 }
